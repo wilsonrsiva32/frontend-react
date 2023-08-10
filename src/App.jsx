11f513277042c42c5f32e29b515a312312c3a955
Wilson <wilson.rsilva345@gmail.com>
2023-08-10 16:42:23 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+function App (){
+  return(   
+    <h1>ola Mundo!</h1>
+
+  )
+}
+
+export default App
